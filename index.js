8b1a2cdca4238bf39c96b01401710f4a8d764494
arif <arifbillahmubin@gmail.com>
2025-11-09 19:40:00 +0600

    server setup done also .env file create database connect done

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000;
+
+//foodLoversDB
+//q0mUPqpLEMGSnhrC
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0fnk2l.mongodb.net/?appName=Cluster0`;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//client create..
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running...')
+})
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("food-lovers-db")
+        const userCollection = db.collection('users');
+
+
+        //user all api...
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const email = req.body.email;
+            const query = { email: email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                res.send({ message: "User already exist , do not insert again.." })
+            } else {
+                const result = await userCollection.insertOne(newUser);
+                res.send(result);
+            }
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        
+    }
+}
+run().catch(console.dir)
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
